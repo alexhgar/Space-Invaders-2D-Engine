f96134419bbbf5f5bdceb5f371c61df1f5852d36
Erik Vazquez <erikk718@gmail.com>
2026-03-06 22:03:44 +0100

    -Actualizado ViewFactory con los metodos mostrarPantallaInicio() y mostrarPantallaJuego(). -gameview.fxml diseñado segun el enunciado y con fondo negro para simular el espacio. -BorderPane y GridPane enlazados en GameController a la espera de que Alex haga su parte. -Limpeza de GameApplication y arranque del juego

--- src/main/java/alexjulenerik/demo/GameApplication.java
+++ src/main/java/alexjulenerik/demo/GameApplication.java
@@ -1,5 +1,6 @@
 package alexjulenerik.demo;
 
+import alexjulenerik.demo.view.ViewFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,10 +11,8 @@ import java.io.IOException;
 public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("gameview.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        ViewFactory.mostrarPantallaInicio();
+
     }
 }
