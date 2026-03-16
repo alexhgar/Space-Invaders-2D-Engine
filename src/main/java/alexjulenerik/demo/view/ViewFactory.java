4ed521ed45a1a2d85226e691b4874bf6b55d35b5
Erik <erikk718@gmail.com>
2026-03-16 17:53:47 +0100

    Modificacion de la ventana para que aparezca toda la matriz

--- src/main/java/alexjulenerik/demo/view/ViewFactory.java
+++ src/main/java/alexjulenerik/demo/view/ViewFactory.java
@@ -23,7 +23,7 @@ public class ViewFactory {
         //Cargar el gameview
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("gameview.fxml"));
         //Tamaño 600x1000 ya que 60x100 se veria muy pequeño (mismas proporciones)
-        Scene scene = new Scene(fxmlLoader.load(),1000,600);
+        Scene scene = new Scene(fxmlLoader.load(),1100,600);
         //Crear el stage
         Stage stage = new Stage();
         stage.setTitle("Space Invaders");
