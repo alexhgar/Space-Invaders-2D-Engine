65d93ec62a4383db3d9b47030f06d88a2ebd5740
Erik <erikk718@gmail.com>
2026-03-23 20:45:45 +0100

    Comentarios en GameController

--- src/main/java/alexjulenerik/demo/GameApplication.java
+++ src/main/java/alexjulenerik/demo/GameApplication.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class   GameApplication extends Application {
+public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         ViewFactory.mostrarPantallaInicio();
