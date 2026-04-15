1f19c49a8161e2c07eddeda084c48675d10c4245
Erik <erikk718@gmail.com>
2026-04-15 16:55:28 +0200

    Cambios en inicio-view.fxml y InicioController para los botones de los tipos de nave

--- src/main/java/alexjulenerik/demo/controller/InicioController.java
+++ src/main/java/alexjulenerik/demo/controller/InicioController.java
@@ -4,76 +4,50 @@ import alexjulenerik.demo.model.GameModel;
 import alexjulenerik.demo.view.ViewFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class InicioController implements Initializable {
+public class InicioController {
 
     @FXML
     private Button btnJugar;
 
     @FXML
-    private Button btnGreen;
+    private ToggleButton btnGreen;
 
     @FXML
-    private Button btnBlue;
+    private ToggleButton btnBlue;
 
     @FXML
-    private Button btnRed;
+    private ToggleButton btnRed;
 
     private String tipoNaveElegida = "GREEN";
 
     private static final GameModel modelo = GameModel.getInstance();
 
-    @Override
-    public void initialize(URL url, ResourceBundle resources) {
-        // Al arrancar, marcamos el botón GREEN como seleccionado
-        actualizarEstilos();
-    }
+
 
 
     @FXML
     public void seleccionarGreen(ActionEvent event){
         tipoNaveElegida = "GREEN";
-        actualizarEstilos();
     }
 
     @FXML
     public void seleccionarBlue(ActionEvent event){
         tipoNaveElegida = "BLUE";
-        actualizarEstilos();
     }
 
     @FXML
     public void seleccionarRed(ActionEvent event){
         tipoNaveElegida = "RED";
-        actualizarEstilos();
     }
 
-    private void actualizarEstilos(){
-        // 1. EL RESET: Devolvemos todos los botones a su estado normal (gris y plano)
-        String estiloApagado = "-fx-background-color: #D3D3D3; -fx-translate-y: 0; -fx-effect: null; -fx-cursor: hand;";
-        btnGreen.setStyle(estiloApagado);
-        btnBlue.setStyle(estiloApagado);
-        btnRed.setStyle(estiloApagado);
-        //Aplicamos el efecto "hundido" y color al seleccionado
-        String estiloActivo = "-fx-translate-y: 0; -fx-cursor: hand; -fx-font-weight: bold; -fx-text-fill: white; -fx-border-width: 2px; -fx-border-radius: 3px; -fx-background-radius: 3px;";
-        switch (tipoNaveElegida) {
-            case "GREEN" -> {
-                btnGreen.setStyle(estiloActivo + "-fx-background-color: #32CD32; -fx-border-color: #00FF00;");
-            }
-            case "BLUE"  -> {
-                btnBlue.setStyle(estiloActivo + "-fx-background-color: #1E90FF; -fx-border-color: #00BFFF;");
-            }
-            case "RED"   -> {
-                btnRed.setStyle(estiloActivo + "-fx-background-color: #FF0000; -fx-border-color: #FF4500;");
-            }
-        }
-    }
 
 
     @FXML
